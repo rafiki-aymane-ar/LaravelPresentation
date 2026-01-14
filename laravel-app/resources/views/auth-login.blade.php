8643d95003c063890ec52d9d107c684e27b1c873
meriemmoukrim <meriemmoukrim@users.noreply.github.com>
2026-01-14 16:07:25 +0100

    Ajout du bouton Retour au cours sur les pages de connexion et d'inscription

--- laravel-app/resources/views/auth-login.blade.php
+++ laravel-app/resources/views/auth-login.blade.php
@@ -82,6 +82,15 @@
             <p class="auth-footer">
                 Pas encore de compte ? <a href="#" onclick="toggleAuth('register')">S'inscrire</a>
             </p>
+
+            <a href="{{ url('/') }}" class="link-back">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="19" y1="12" x2="5" y2="12"></line>
+                    <polyline points="12 19 5 12 12 5"></polyline>
+                </svg>
+                <span>Retour au cours</span>
+            </a>
         </div>
 
         <div class="auth-box" id="register-box" style="display: none;">
@@ -135,6 +144,15 @@
             <p class="auth-footer">
                 Déjà un compte ? <a href="#" onclick="toggleAuth('login')">Se connecter</a>
             </p>
+
+            <a href="{{ url('/') }}" class="link-back">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <line x1="19" y1="12" x2="5" y2="12"></line>
+                    <polyline points="12 19 5 12 12 5"></polyline>
+                </svg>
+                <span>Retour au cours</span>
+            </a>
         </div>
         </div>
     @endauth
